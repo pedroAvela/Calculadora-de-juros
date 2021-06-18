43b6bddb2a811bfb22f9435178c30e3f60eecf88
under_taker <82622259+pedroAvela@users.noreply.github.com>
2021-06-18 00:41:20 +0000

    Initial commit

---
+++ invest.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+struct tInvestimento{
+  int montInvestido, periodoAno;
+  float juroAno;
+};
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "invest.cpp"
+
+using namespace std;
+
+int main() {
+  
+  tInvestimento teste;
+
+  teste.montInvestido = 500;
+
+  cout << teste.montInvestido << endl;
+}
