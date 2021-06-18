bd94a71b76477490e73a760fd376c1c2fbbcf0f0
Pedro Henrique Avelar Borges <82622259+pedroAvela@users.noreply.github.com>
2021-06-18 17:04:08 +0000

    Criei um menu para o usuário, e um método para repetir o processo.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
-#include <math.h>
 #include "invest.cpp"
 
 using namespace std;
 
-
-
 int main() {
-  
   tInvestimento teste;
+  bool is_playing = true;
+  char choice;
 
-  cout << "Digite o montante investido:" << endl;
-  cin >> teste.montInvestido;
-  cout << "Digite o juro ao ano:" << endl;
-  cin >> teste.juroAno;
-  cout << "Digite o periodo em anos que o dinheiro ficará investido:" << endl;
-  cin >> teste.periodoAno;
+  while (is_playing){
+    bool is_choosing = true;
+     
+    cout << "Digite o montante investido:" << endl;
+    cin >> teste.montInvestido;
+    cout << "Digite o juro ao ano:" << endl;
+    cin >> teste.juroAno;
+    cout << "Digite o periodo em anos que o dinheiro ficará investido:" << endl;
+    cin >> teste.periodoAno;
 
-  teste.calculoJuros();
-  teste.conversorJuro();
-  teste.imprimirValores();
+    teste.calculoJuros();
+    teste.conversorJuro();
+    teste.imprimirValores();
 
+    cout << "Deseja fazer calcular mais algum valor? y para sim, n para não" << endl;
+    cin >> choice;
+
+    while (is_choosing){
+      if (choice == 'n'){
+        is_playing = false;
+        is_choosing = false;
+      }else if(choice == 'y'){
+        is_choosing = false;
+      }else{
+        cout << "Opção inválida, tente de novo, y para sim n para não" << endl;
+        cin >> choice;
+      }
+    }
+
+  }
+
+  return 0;
 }
