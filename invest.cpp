cf2c468b1943aa1b18259fd5e7e84e707bec8b8c
Pedro Henrique Avelar Borges <82622259+pedroAvela@users.noreply.github.com>
2021-06-18 16:37:16 +0000

    Adicionei uma função que converte o juros ao ano para ao mês e uma função para imprimir os valores

--- invest.cpp
+++ invest.cpp
@@ -4,14 +4,30 @@
 using namespace std;
 
 struct tInvestimento{
-  float montInvestido, juroAno, periodoAno, montFinalSimples, montFinalComposto;
+  float montInvestido, juroAno, periodoAno, montFinalSimples, montFinalComposto, juroMes;
 
   void calculoJuros(){
     montFinalSimples = montInvestido * (1 + ((juroAno/100) * periodoAno));
     montFinalComposto = montInvestido * pow(1+(juroAno/100), periodoAno);
 
+    //cout << montFinalSimples << endl;
+    //cout << montFinalComposto << endl;
+  }
+
+  void conversorJuro(){
+    
+    float taxaMes, taxaAno = (float(juroAno))/100, aux = 1.0/12;
+
+    taxaMes = pow(1 + taxaAno, aux) - 1;
+    juroMes = taxaMes * 100;
+
+    //cout << juroMes << "%" << endl;
+  }
+
+  void imprimirValores(){
     cout << montFinalSimples << endl;
     cout << montFinalComposto << endl;
+    cout << juroMes << "%" << endl;        
   }
 
 };
--- main.cpp
+++ main.cpp
@@ -11,9 +11,11 @@ int main() {
   tInvestimento teste;
 
   teste.montInvestido = 1000;
-  teste.juroAno = 5;
+  teste.juroAno = 25;
   teste.periodoAno = 2;
 
   teste.calculoJuros();
+  teste.conversorJuro();
+  teste.imprimirValores();
 
 }
