f8df830d4e2971b28517b5140a577022b4dc1f17
Pedro Henrique Avelar Borges <82622259+pedroAvela@users.noreply.github.com>
2021-06-18 18:44:11 +0000

    Melhorei a experiência do usuário, e adicionei um verificador para garantir que caso não seja digitado um número, o código não bugue

--- invest.cpp
+++ invest.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
 
 struct tInvestimento{
-  float montInvestido, juroAno, periodoAno, montFinalSimples, montFinalComposto, juroMes;
+  float montInvestido, juroAno, periodoAno, montFinalSimples, montFinalComposto, juroMes, taxaAno;
 
   void calculoJuros(){
-    montFinalSimples = montInvestido * (1 + ((juroAno/100) * periodoAno));
-    montFinalComposto = montInvestido * pow(1+(juroAno/100), periodoAno);
+    taxaAno = (float(juroAno))/100;
+    montFinalSimples = montInvestido * (1 + (taxaAno * periodoAno));
+    montFinalComposto = montInvestido * pow(1+taxaAno, periodoAno);
 
-    //cout << montFinalSimples << endl;
-    //cout << montFinalComposto << endl;
   }
 
-  void conversorJuro(){
-    
-    float taxaMes, taxaAno = (float(juroAno))/100, aux = 1.0/12;
+  void conversorJuro(){ 
+    float taxaMes, aux = 1.0/12;
 
     taxaMes = pow(1 + taxaAno, aux) - 1;
     juroMes = taxaMes * 100;
 
-    //cout << juroMes << "%" << endl;
   }
 
   void imprimirValores(){
-    cout << montFinalSimples << endl;
-    cout << montFinalComposto << endl;
-    cout << juroMes << "%" << endl;        
+    cout << fixed << setprecision(2);
+
+    cout << "\nMontante final com juro simples: " << montFinalSimples << endl;
+    cout << "Montante final com juro composto: " << montFinalComposto << endl;
+
+    cout << fixed << setprecision(4);
+    cout << "Juro ao mes: " << juroMes << "% a.m" << endl;        
   }
 
 };
--- main.cpp
+++ main.cpp
@@ -1,28 +1,63 @@
 #include <iostream>
+#include <string>
 #include "invest.cpp"
 
 using namespace std;
+
+bool verificador(string montante, string juro, string periodo){
+  if ((montante[0] > 47) && (montante[0] < 58)){
+    if ((juro[0] > 47) && (juro[0] < 58)){
+      if ((periodo[0] > 47) && (periodo[0] < 58)){
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
 
 int main() {
   tInvestimento teste;
   bool is_playing = true;
   char choice;
 
+  cout << R"(
+     ___        _               _             _                          _          __                          
+    / __\ __ _ | |  ___  _   _ | |  __ _   __| |  ___   _ __  __ _    __| |  ___    \ \  _   _  _ __  ___   ___ 
+   / /   / _` || | / __|| | | || | / _` | / _` | / _ \ | '__|/ _` |  / _` | / _ \    \ \| | | || '__|/ _ \ / __|
+  / /___| (_| || || (__ | |_| || || (_| || (_| || (_) || |  | (_| | | (_| ||  __/ /\_/ /| |_| || |  | (_) |\__ \
+  \____/ \__,_||_| \___| \__,_||_| \__,_| \__,_| \___/ |_|   \__,_|  \__,_| \___| \___/  \__,_||_|   \___/ |___/ by Pedro Avelar
+  )" << "\n";
+
   while (is_playing){
     bool is_choosing = true;
-     
+    string montante, juro, periodo;
     cout << "Digite o montante investido:" << endl;
-    cin >> teste.montInvestido;
+    cin >> montante;
     cout << "Digite o juro ao ano:" << endl;
-    cin >> teste.juroAno;
+    cin >> juro;
     cout << "Digite o periodo em anos que o dinheiro ficará investido:" << endl;
-    cin >> teste.periodoAno;
+    cin >> periodo;
+
+    if (verificador(montante, juro, periodo) == false){
+      cout << "\033[2J\033[1;1H";
+      cout << "Números inválidos" << endl;
+      continue;
+    }
+
+    teste.montInvestido = stof(montante);
+    teste.juroAno = stof(juro);
+    teste.periodoAno = stof(periodo);
 
     teste.calculoJuros();
     teste.conversorJuro();
     teste.imprimirValores();
 
-    cout << "Deseja fazer calcular mais algum valor? y para sim, n para não" << endl;
+    cout << "\nDeseja fazer calcular mais algum valor? y para sim, n para não" << endl;
     cin >> choice;
 
     while (is_choosing){
@@ -30,6 +65,7 @@ int main() {
         is_playing = false;
         is_choosing = false;
       }else if(choice == 'y'){
+        cout << "\033[2J\033[1;1H";
         is_choosing = false;
       }else{
         cout << "Opção inválida, tente de novo, y para sim n para não" << endl;
